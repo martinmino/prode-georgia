acae2ac35556dcb663d4f57e76b6186f71ea037f
Matias <Matias@georgia.com.ar>
2022-05-20 09:30:49 -0300

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,37 +1,35 @@
 <x-app-layout>
-    <div class="col-sm-12">
-        <h2>Dashboard is works!</h2>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam fugiat recusandae, maxime beatae vitae voluptas! Ut asperiores qui labore molestiae fuga sunt. Excepturi, similique aliquam itaque quam amet reiciendis modi.</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam fugiat recusandae, maxime beatae vitae voluptas! Ut asperiores qui labore molestiae fuga sunt. Excepturi, similique aliquam itaque quam amet reiciendis modi.</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam fugiat recusandae, maxime beatae vitae voluptas! Ut asperiores qui labore molestiae fuga sunt. Excepturi, similique aliquam itaque quam amet reiciendis modi.</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam fugiat recusandae, maxime beatae vitae voluptas! Ut asperiores qui labore molestiae fuga sunt. Excepturi, similique aliquam itaque quam amet reiciendis modi.</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam fugiat recusandae, maxime beatae vitae voluptas! Ut asperiores qui labore molestiae fuga sunt. Excepturi, similique aliquam itaque quam amet reiciendis modi.</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam fugiat recusandae, maxime beatae vitae voluptas! Ut asperiores qui labore molestiae fuga sunt. Excepturi, similique aliquam itaque quam amet reiciendis modi.</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam fugiat recusandae, maxime beatae vitae voluptas! Ut asperiores qui labore molestiae fuga sunt. Excepturi, similique aliquam itaque quam amet reiciendis modi.</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam fugiat recusandae, maxime beatae vitae voluptas! Ut asperiores qui labore molestiae fuga sunt. Excepturi, similique aliquam itaque quam amet reiciendis modi.</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam fugiat recusandae, maxime beatae vitae voluptas! Ut asperiores qui labore molestiae fuga sunt. Excepturi, similique aliquam itaque quam amet reiciendis modi.</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam fugiat recusandae, maxime beatae vitae voluptas! Ut asperiores qui labore molestiae fuga sunt. Excepturi, similique aliquam itaque quam amet reiciendis modi.</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam fugiat recusandae, maxime beatae vitae voluptas! Ut asperiores qui labore molestiae fuga sunt. Excepturi, similique aliquam itaque quam amet reiciendis modi.</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam fugiat recusandae, maxime beatae vitae voluptas! Ut asperiores qui labore molestiae fuga sunt. Excepturi, similique aliquam itaque quam amet reiciendis modi.</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam fugiat recusandae, maxime beatae vitae voluptas! Ut asperiores qui labore molestiae fuga sunt. Excepturi, similique aliquam itaque quam amet reiciendis modi.</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam fugiat recusandae, maxime beatae vitae voluptas! Ut asperiores qui labore molestiae fuga sunt. Excepturi, similique aliquam itaque quam amet reiciendis modi.</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam fugiat recusandae, maxime beatae vitae voluptas! Ut asperiores qui labore molestiae fuga sunt. Excepturi, similique aliquam itaque quam amet reiciendis modi.</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam fugiat recusandae, maxime beatae vitae voluptas! Ut asperiores qui labore molestiae fuga sunt. Excepturi, similique aliquam itaque quam amet reiciendis modi.</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam fugiat recusandae, maxime beatae vitae voluptas! Ut asperiores qui labore molestiae fuga sunt. Excepturi, similique aliquam itaque quam amet reiciendis modi.</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam fugiat recusandae, maxime beatae vitae voluptas! Ut asperiores qui labore molestiae fuga sunt. Excepturi, similique aliquam itaque quam amet reiciendis modi.</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam fugiat recusandae, maxime beatae vitae voluptas! Ut asperiores qui labore molestiae fuga sunt. Excepturi, similique aliquam itaque quam amet reiciendis modi.</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam fugiat recusandae, maxime beatae vitae voluptas! Ut asperiores qui labore molestiae fuga sunt. Excepturi, similique aliquam itaque quam amet reiciendis modi.</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam fugiat recusandae, maxime beatae vitae voluptas! Ut asperiores qui labore molestiae fuga sunt. Excepturi, similique aliquam itaque quam amet reiciendis modi.</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam fugiat recusandae, maxime beatae vitae voluptas! Ut asperiores qui labore molestiae fuga sunt. Excepturi, similique aliquam itaque quam amet reiciendis modi.</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam fugiat recusandae, maxime beatae vitae voluptas! Ut asperiores qui labore molestiae fuga sunt. Excepturi, similique aliquam itaque quam amet reiciendis modi.</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam fugiat recusandae, maxime beatae vitae voluptas! Ut asperiores qui labore molestiae fuga sunt. Excepturi, similique aliquam itaque quam amet reiciendis modi.</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam fugiat recusandae, maxime beatae vitae voluptas! Ut asperiores qui labore molestiae fuga sunt. Excepturi, similique aliquam itaque quam amet reiciendis modi.</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam fugiat recusandae, maxime beatae vitae voluptas! Ut asperiores qui labore molestiae fuga sunt. Excepturi, similique aliquam itaque quam amet reiciendis modi.</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam fugiat recusandae, maxime beatae vitae voluptas! Ut asperiores qui labore molestiae fuga sunt. Excepturi, similique aliquam itaque quam amet reiciendis modi.</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam fugiat recusandae, maxime beatae vitae voluptas! Ut asperiores qui labore molestiae fuga sunt. Excepturi, similique aliquam itaque quam amet reiciendis modi.</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam fugiat recusandae, maxime beatae vitae voluptas! Ut asperiores qui labore molestiae fuga sunt. Excepturi, similique aliquam itaque quam amet reiciendis modi.</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam fugiat recusandae, maxime beatae vitae voluptas! Ut asperiores qui labore molestiae fuga sunt. Excepturi, similique aliquam itaque quam amet reiciendis modi.</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam fugiat recusandae, maxime beatae vitae voluptas! Ut asperiores qui labore molestiae fuga sunt. Excepturi, similique aliquam itaque quam amet reiciendis modi.</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam fugiat recusandae, maxime beatae vitae voluptas! Ut asperiores qui labore molestiae fuga sunt. Excepturi, similique aliquam itaque quam amet reiciendis modi.</p>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            @if(session('mensaje'))
+            {{session('mensaje')}}
+            @endif
+            @foreach ($partido as $partidos)
+                <form action="{{route('dashboard.store')}}" method="POST">
+            @csrf
+                    <ul>
+                        <li>
+                            <label >
+                                {{$partidos->Paises->name}}
+                                <input type="number"  name="paisa{{$partidos->id}}" min="0" max ="99" value="{{old("paisa$partidos->id")}}" required>
+                                <input type="number" name="paisb{{$partidos->id}}"  min="0" max = "99" value="{{old("paisb$partidos->id")}}"required>
+                                <input name="id" type="hidden" value={{$partidos->id}}>
+                                {{$partidos->Paises2->name}}
+                            </label>
+                        </li>
+                        <br>
+                        <button>Enviar</button>
+                    </ul>
+                    @error("paisa$partidos->id")
+                            {{$message}}
+                        @enderror
+                </form>
+            @endforeach
+        </div>
     </div>
 </x-app-layout>
