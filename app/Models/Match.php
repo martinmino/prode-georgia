f9aaa3eb823b3d3da65771b5568485ddfba85995
Martin Miño <martin77mino@gmail.com>
2022-05-20 07:43:14 -0300

    Update Match.php

--- app/Models/Match.php
+++ app/Models/Match.php
@@ -4,10 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Country;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Match extends Model
 {
     use HasFactory;
 
     public $timestamps = false;
+
+    /**
+     * Get the country that owns the Match
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function country1(): BelongsTo
+    {
+        return $this->belongsTo(Country::class, 'country1_id', 'id');
+    }
+    public function country2(): BelongsTo
+    {
+        return $this->belongsTo(Country::class, 'country2_id', 'id');
+    }
 }
