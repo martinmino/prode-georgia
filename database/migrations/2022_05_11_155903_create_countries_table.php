0c67dc1670f68f88ac9aaad72377e1d5083ed357
Martin Miño <Matias@georgia.com.ar>
2022-05-30 09:13:23 -0300

    Update 2022_05_11_155903_create_countries_table.php

--- database/migrations/2022_05_11_155903_create_countries_table.php
+++ database/migrations/2022_05_11_155903_create_countries_table.php
@@ -16,6 +16,7 @@ class CreateCountriesTable extends Migration
         Schema::create('countries', function (Blueprint $table) {
             $table->id();
             $table->string('name', 50);
+            $table->string('iso', 6);
             $table->timestamps();
         });
     }
