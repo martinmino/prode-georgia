35fa1822c650830fca7177d629d6d3896afdd4a3
Matias <Matias@georgia.com.ar>
2022-06-02 10:21:58 -0300

    Create Match.php

---
+++ app/Models/Match.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Country;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Partido extends Model
+{
+    use HasFactory;
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'date',
+        'time',
+        'group',
+        'phase',
+        'country1_id',
+        'country2_id',
+        'goals1',
+        'goals2',
+        'active_since',
+        'is_over'
+    ];
+
+    /**
+     * Get the country that owns the Match
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function country1(): BelongsTo
+    {
+        return $this->belongsTo(Country::class, 'country1_id', 'id');
+    }
+    public function country2(): BelongsTo
+    {
+        return $this->belongsTo(Country::class, 'country2_id', 'id');
+    }
+}
