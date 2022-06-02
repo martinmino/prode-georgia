09dc1fa44011a240a21b474d01b636beefd91d75
Matias <Matias@georgia.com.ar>
2022-06-02 15:08:24 -0300

    Update pronostic-card.blade.php

--- resources/views/livewire/pronostic-card.blade.php
+++ resources/views/livewire/pronostic-card.blade.php
@@ -9,20 +9,22 @@
                     <label class="card-text" style="margin-left:15px; margin-right:5px;">
                         <b>{{strtoupper(substr($country1->name,0,3))}}</b>
                     </label>
-                    <input type="number" name="goals1" min="0" max ="99" wire:model.defer="goals1" required style="width : 40px; heigth : 1px; margin-right:10px;">
+                    <input type="number" name="goals1" min="0" max ="99" wire:model.defer="goals1" required style="width : 40px; heigth : 1px; margin-right:10px;"{{$hoy>$match->date?'disabled':''}}>
 
                     <label class="card-text" style="margin-left:5px; margin-right:10px;">
                         <b>VS</b>
                     </label>
 
-                    <input type="number" name="goals2"  min="0" max = "99" wire:model.defer="goals2" required style="width : 40px; heigth : 1px ">
+                    <input type="number" name="goals2"  min="0" max = "99" wire:model.defer="goals2" required style="width : 40px; heigth : 1px " {{$hoy>$match->date?'disabled':''}}>
                     <label class="card-text" style="margin-left:5px; margin-right:10px;">
                         <b>{{strtoupper(substr($country2->name,0,3))}}</b>
                     </label>
                 </div>
                 <br>
                 <br>
-                <button class="btn btn-outline-dark" wire:click="grabar">Enviar</button>
+                @if ($match->date > $hoy)
+                    <button class="btn btn-outline-dark" wire:click="grabar">Enviar</button>
+                @endif
                 <br>
                 <br>
                 <p style = "float: left; margin-right: 20px; "><b> Fecha: {{Carbon\Carbon::parse($match->date)->format('d/m/Y') }}</b></p><p > <b>Horario: {{ $match->time }}hs</b></p>
