2590a9c216aaa7d832d77b9021d303429b1ea4ab
Matias <Matias@georgia.com.ar>
2022-06-03 14:17:27 -0300

    Update positions.blade.php

--- resources/views/positions.blade.php
+++ resources/views/positions.blade.php
@@ -1,5 +1,10 @@
-<x-app-layout>
-    <div class="py-12">
+<x-app-layout >
+    <div class="py-12"  style="max-width: 1280px;
+    width: 100%;
+    position: relative;
+    display: block;
+    margin: 0 auto;
+    margin-top: 1cm">
            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                    <h2>Positions is works!</h2>
