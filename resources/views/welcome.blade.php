9a53b693ab9e91c102704a0dcf8dff312494f6f5
Matias <Matias@georgia.com.ar>
2022-06-03 14:17:31 -0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,10 @@
 <x-app-layout>
-    <div class="col-sm-12">
+    <div class="col-sm-12"  style="max-width: 1280px;
+    width: 100%;
+    position: relative;
+    display: block;
+    margin: 0 auto;
+    margin-top: 1cm">
         <h2>Welcome is works!</h2>
 
     </div>
