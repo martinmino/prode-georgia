c3adb3e3548d6a7b41eea3f1b1e2a1248af58026
Matias <Matias@georgia.com.ar>
2022-06-03 11:38:25 -0300

    Update regulation.blade.php

--- resources/views/regulation.blade.php
+++ resources/views/regulation.blade.php
@@ -1,9 +1,49 @@
 <x-app-layout>
-    <div class="py-12">
+    <div class="py-12" style="    max-width: 1280px;
+    width: 100%;
+    position: relative;
+    display: block;
+    margin: 0 auto;">
            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <h2>Regulation is works!</h2>
+                <h1 style="text-align: center;   margin-bottom: 35px;  border-bottom: 2px solid black; padding: 20px 0 30px;">Reglamento</h1>
+                <h2 style=" display: block; font-size: 17px; font-weight: 500; margin-bottom: 6px;">1. Objetivo del juego</h2>
+                <p style="line-height: 21px;">En el juego cada participante pronostica resultados de los partidos de fútbol cargados en la plataforma. El objetivo es acumular la mayor cantidad de puntos en base al acierto de pronósticos de estos partidos. El jugador que sume mayor cantidad de puntos en una fecha será el ganador de dicha fecha y el jugador que sume la mayor cantidad de puntos a lo largo de todo el torneo será el ganador del juego.</p>
+                <br>
+                <hr>
+                <h2 style=" display: block; font-size: 17px; font-weight: 500; margin-bottom: 6px;">2. Desarrollo</h2>
+                <p style="line-height: 21px;">Dentro del juego el participante puede comenzar a pronosticar los partidos de las fechas habilitadas del torneo que se esté desarrollando. El participante tiene hasta comenzado el partido para pronosticar el resultado, de lo contrario, una vez comenzado el partido, no podrá colocar resultados y por lo tanto no obtendrá puntos. Los puntos que obtiene el participante, una vez finalizado el partido, estarán dados de la siguiente forma:
+                <br>
+                <br>
+                <ul>
+                <li> Acierto de resultado y marcador (cantidad de goles de cada equipo) exactos: 9 puntos + 3 puntos adicionales = 12 puntos.</li>
+                <br>
+                <li>Acertar resultado (ganador o empate no exacto): 5 puntos.</li>
+                <br>
+                <li>Acertar marcador (cantidad de goles) de uno de los equipos: 2 puntos.</li>
+                <br>
+                <br>
+                </ul>
+                Los resultados podrán pronosticarse y ser modificados hasta una hora antes del comienzo del partido.
+                </p>
+                <br>
+                <hr>
+                <h2 style=" display: block; font-size: 17px; font-weight: 500; margin-bottom: 6px;">3. Ejemplos sobre cómo sumar puntos:</h2>
+                <p style="line-height: 21px;"><strong>Resultado oficial: Equipo A 3 vs. Equipo B 1</strong><br>Predicción 3 - 1: 12 puntos. Acertó el resultado y marcador exacto.<br>Predicción 2 - 0: 5 puntos. Acertó ganador pero no el marcador de ninguno de los equipos.<br>Predicción 3 - 0: 7 puntos. Acertó ganador y cantidad de goles de un equipo.<br>Predicción 2 - 1: 7 puntos. Acertó ganador y cantidad de goles de un equipo.<br>Predicción 0 - 1: 2 puntos. No acertó ganador pero si la cantidad de goles de uno de los equipos.<br>Predicción 3 - 3: 2 puntos. No acertó ganador pero si la cantidad de goles de uno de los equipos.<br>Predicción 0 - 0: 0 puntos. No hay ningún acierto.<br></p>
+                <br>
+                <hr>
+                 <h2 style=" display: block; font-size: 17px; font-weight: 500; margin-bottom: 6px;">4. Tu final Soñada</h2>
+                 <p style="line-height: 21px;"><strong>4.1</strong> En el formulario de inscripción el participante deberá responder a 4 preguntas relacionadas a los 4 finalistas del torneo.<br><strong>4.1.1</strong> Se le otorgarán 10 (diez) puntos adicionales por cada acierto de equipos que ocupen las cuatro primeras posiciones.<br><strong>4.1.2</strong> Los participantes no podrán repetir los países en ninguna posición, ejemplo: 1er puesto del torneo, EQUIPO A; 2do puesto, EQUIPO A; 3er puesto, EQUIPO A; 4to puesto, EQUIPO A.<br><strong>4.1.3</strong> Los participantes tendrán tiempo a cambiar los 4 finalistas del torneo hasta que termine la primera fase de grupos.<br></p>
+                 <br>
+                <hr>
+                 <h2 style=" display: block; font-size: 17px; font-weight: 500; margin-bottom: 6px;">5. Penales</h2>
+                 <p style="line-height: 21px;"><strong>5.1</strong> Dependiendo de la estructura del torneo, el jugador podrá pronosticar resultados con empates y seleccionar un ganador por penales en caso de que corresponda. Si su marcador final es "empate" (1-1, 2-2, 3-3, etc.) se le desplegará una barra para que seleccione sólo al ganador de los penales sin importar la cantidad de goles.<br><br><strong>Resultado oficial (a partir de Cuartos de final): EQUIPO A 1 vs. EQUIPO B 1 = Van a penales = EQUIPO A gana el partido por penales.</strong><br>Predicción 1 - 1 (y ganador por penales EQUIPO A): 17 puntos 12 por acertar el resultado exacto del partido más 5 puntos por acertar el ganador de los penales.<br>Predicción 1 - 1 (y ganador por penales EQUIPO B): 12 puntos 12 por acertar el resultado exacto del partido.<br>Predicción 0 - 0 (y ganador por penales EQUIPO A): 10 puntos 5 puntos por acertar el marcador del partido (empate), 5 puntos por acertar el ganador de los penales.<br>Predicción 0 - 0 (y ganador por penales EQUIPO B): 5 puntos 5 puntos por acertar el marcador del partido (empate).</p>
+                 <br>
+                <hr>
+                 <h2 style=" display: block; font-size: 17px; font-weight: 500; margin-bottom: 6px;">6. Situación de empate</h2>
+                 <p style="line-height: 21px;">En caso de empate, se posicionará primero al participante que:<br><br><strong>PRIMERA CLAUSULA:</strong> Haya acertado la mayor cantidad de resultados exactos en la fecha<br><strong>SEGUNDA CLAUSULA:</strong> Haya acertado la mayor cantidad de resultados exactos en en el torneo<br><strong>TERCERA CLAUSULA:</strong> Se haya registrado al juego antes de aquellos que obtuvieron el mismo puntaje y mismos resultados exactos.<br></p>
                </div>
            </div>
        </div>
    </x-app-layout>
+
