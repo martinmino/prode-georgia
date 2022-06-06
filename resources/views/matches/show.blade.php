ab0d89529d1e1e15178b4ddc5d511729e0bb0457
Matias <Matias@georgia.com.ar>
2022-06-06 10:40:15 -0300

    Update show.blade.php

--- resources/views/matches/show.blade.php
+++ resources/views/matches/show.blade.php
@@ -1,5 +1,10 @@
 <x-app-layout>
-    <div class="row">
+    <div class="row"style="max-width: 1280px;
+    width: 100%;
+    position: relative;
+    display: block;
+    margin: 0 auto;
+    margin-top: 1cm" >
         <div class="col-8">
             <h1>
                 {{ $match->country1->name }}
@@ -8,9 +13,10 @@
                 <span class="badge bg-secondary">{{ $match->goals2}}</span>
                 {{ $match->country2->name }}
             </h1>
+            <a href="{{ route('matches.index')}}" class="btn btn-primary">Volver</a>
         </div>
-        <div class="col-2">
 
+        <div class="col-2">
         </div>
     </div>
 
