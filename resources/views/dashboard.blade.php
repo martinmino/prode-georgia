af3bea1d2eecc548e44b93bae2cefc14b7681b5b
Martin Miño <martin77mino@gmail.com>
2022-06-24 12:04:27 -0300

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,14 +1,10 @@
 <x-app-layout>
     @livewire('pronostico-final', ['user' => Auth::user()])
 
-    <h1>Aqui los pronosticos</h1>
-
     <div class="row">
-        <div class="card-group">
-            @foreach ($pronostics as $pronostic)
-                @livewire('pronostic-card', ['pronostic' => $pronostic])
-            @endforeach
-        </div>
+        @foreach ($pronostics as $pronostic)
+            @livewire('pronostic-card', ['pronostic' => $pronostic])
+        @endforeach
     </div>
 
 </x-app-layout>
